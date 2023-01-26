b1eebba7b3aaa93db1d71022735c8233f7aba5ff
Behenck <denilsontrespa10@gmail.com>
2023-01-26 11:56:31 -0300

    maintenance

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: 'intranet.santacasa.com.br',
     port: 3332,
   },
   plugins: [react()],
